9d06fb4d5b3b63b63444dd2a4f510a8c96fc3dbf
mohammed EZZARIY <alcazar.quivir.mez@gmail.com>
2023-03-27 16:34:56 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *reset_to_98 - function takes the pointer and updates the value
+ *
+ *@n: integer being tested
+ *
+ *Return: 0 on success
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
